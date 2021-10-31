984629252e4d769d81dfd442c98bf82bc662f6b6
Gonza2324 <91620631+Gonza2324@users.noreply.github.com>
2021-10-31 14:54:40 -0500

    Agregue un comentario
    porque quiero

--- Integral_definida.py
+++ Integral_definida.py
@@ -1,6 +1,6 @@
 from sympy import integrate
 from sympy.abc import x, C
-
+#Hola hice un cambio
 funcion = input('Ingrese la funcion: ')
 a = input('Ingrese el valor "a": ')
 b = input('Ingrese el valor "b": ')
